6d1afe5a73c5623643c7763e1b74c2fd40257547
JovemCarlosTI <59931270+JovemCarlosTI@users.noreply.github.com>
2023-05-07 22:40:41 +0000

    Adicionando Tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,8 +3,12 @@ const criarTarefa = (e) => {
 	
 	const input = document.querySelector('[data-form-input]');
 	const valor = input.value;
-	console.log(valor)
-	input.value = ""
+
+	const tarefa = document.querySelector('[data-task]');
+	const conteudo = `<p class="content">${valor}</p>`;
+
+	tarefa.innerHTML = conteudo;
+	input.value = "";
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
